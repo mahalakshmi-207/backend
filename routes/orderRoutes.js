5a13acea121cbba258e49a549f070b286bc23371
mahalakshmi-207 <mahalakshmibalamurugan70@gmail.com>
2025-11-15 22:24:53 +0530

    Fix

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,7 +4,7 @@ import {
   getOrderById,
   getMyOrders,
 } from "../controllers/orderController.js";
-import protect from "../middleware/authMiddleware.js";
+import {protect} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
